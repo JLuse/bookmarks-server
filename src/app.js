0db83fe3f8275610d9703cb0216b3c0d84daca0d
Jordan Luse <j.luse7@gmail.com>
2021-02-09 19:09:26 -0800

    Added solution

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-const logger = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require('./logger')
+
+const bookmarkRouter = require('./bookmark/bookmark-router');
 
 const app = express()
 
@@ -11,9 +14,25 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(logger(morganOption))
+app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+// validates that an Authorization header with an API token was present
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+});
+
+app.use(bookmarkRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
